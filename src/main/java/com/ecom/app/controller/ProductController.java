6274763462e8e7937c638b588554d3bfffb1f6eb
vaibhav tripathi <vaibhavtripathi272001lko@gmail.com>
2025-10-03 23:37:05 +0530

    Added Update Product and Update Product controller and services

--- src/main/java/com/ecom/app/controller/ProductController.java
+++ src/main/java/com/ecom/app/controller/ProductController.java
@@ -3,22 +3,32 @@ package com.ecom.app.controller;
 import com.ecom.app.dto.ProductRequest;
 import com.ecom.app.dto.ProductResponse;
 import com.ecom.app.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
     private final ProductService productService;
     @PostMapping
-    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest)
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest)
     {
-        return new ResponseEntity<ProductResponse>(productService.createProduct(ProductRequest),
+        return new ResponseEntity<ProductResponse>(productService.createProduct(productRequest),
                 HttpStatus.CREATED);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(
+            @PathVariable Long id,
+            @RequestBody ProductRequest productRequest)
+    {
+        return productService.updateProduct(id,productRequest)
+                .map(ResponseEntity::ok)
+                .orElseGet(()-> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/ecom/app/service/ProductService.java
+++ src/main/java/com/ecom/app/service/ProductService.java
@@ -1,10 +1,18 @@
 package com.ecom.app.service;
 
 import com.ecom.app.dto.ProductRequest;
+import com.ecom.app.dto.ProductResponse;
 import com.ecom.app.model.Product;
 import com.ecom.app.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.authenticator.SavedRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class ProductService {
     @Autowired
     private final ProductRepository productRepository;
@@ -12,6 +20,39 @@ public class ProductService {
     {
         Product product= new Product();
         updateProductFromRequest(product,productRequest);
-        productRepository.save(product);
+       Product savedProduct= productRepository.save(product);
+       return mapToProductResponse(savedProduct);
+    }
+
+    private void updateProductFromRequest(Product product, ProductRequest productRequest) {
+        product.setName(productRequest.getName());
+        product.setPrice(productRequest.getPrice());
+        product.setCategory(productRequest.getCategory());
+        product.setImageUrl(productRequest.getImageUrl());
+        product.setStockQuantity(productRequest.getStockQuantity());
+        product.setPrice(productRequest.getPrice());
+//        product.setActive(productRequest.getActive());
+    }
+
+    private ProductResponse mapToProductResponse(Product savedProduct) {
+        ProductResponse response= new ProductResponse();
+        response.setId(savedProduct.getId());
+        response.setName(savedProduct.getName());
+        response.setPrice(savedProduct.getPrice());
+        response.setCategory(savedProduct.getCategory());
+        response.setImageUrl(savedProduct.getImageUrl());
+        response.setStockQuantity(savedProduct.getStockQuantity());
+        response.setPrice(savedProduct.getPrice());
+        response.setActive(savedProduct.getActive());
+        return response;
+    }
+
+    public Optional<ProductResponse> updateProduct(Long id, ProductRequest productRequest) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    updateProductFromRequest(existingProduct, productRequest);
+                    Product savedProduct = productRepository.save(existingProduct);
+                    return mapToProductResponse(savedProduct);
+                });
     }
 }
