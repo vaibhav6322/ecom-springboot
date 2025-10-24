a375490b90e0ffedad3c7303e4fc5dac62b9c372
vaibhav tripathi <vaibhavtripathi272001lko@gmail.com>
2025-10-24 22:30:29 +0530

    getAllEndPoint

--- src/main/java/com/ecom/app/controller/CartController.java
+++ src/main/java/com/ecom/app/controller/CartController.java
@@ -2,11 +2,14 @@ package com.ecom.app.controller;
 
 
 import com.ecom.app.dto.CartItemRequest;
+import com.ecom.app.model.Product;
 import com.ecom.app.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/cart")
@@ -23,5 +26,21 @@ public class CartController {
             return ResponseEntity.badRequest().body("Product Out of Stock or User not found or Product not found");
         }
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @DeleteMapping("/items/{productId}")
+    public ResponseEntity<Void> removeFromCart(
+            @RequestHeader("X-User-ID") String userId,
+            @PathVariable Long productId)
+    {
+      boolean deleted=  cartService.deleteItemFromCart(userId,productId);
+      return deleted ? ResponseEntity.noContent().build()
+              : ResponseEntity.notFound().build();
+    }
+    @GetMapping
+    public List<Product> getAllCartProducts(
+            @RequestHeader("X-User-ID") String userId
+    )
+    {
+        return cartService.getAllProductsFromCart(userId);
     }
 }
--- src/main/java/com/ecom/app/repositories/CartItemRepository.java
+++ src/main/java/com/ecom/app/repositories/CartItemRepository.java
@@ -6,8 +6,14 @@ import com.ecom.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     CartItem findByUserAndProduct(User user, Product product);
+
+    void deleteByUserAndProduct(User user, Product product);
+
+    List<CartItem> findAllByUser(User user);
 }
